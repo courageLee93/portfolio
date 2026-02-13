e76f51a5a384ba2df4ff4160e582b04b971b97a7
courageLee93 <couragelee93@gmail.com>
2026-02-13 12:15:26 +0900

    refactor: code update

--- attribution/data.py
+++ attribution/data.py
@@ -14,33 +14,12 @@ indexing_groups(group_columns=["uid", "conversion_id", "campaign"],
 '''
 # </editor-fold>
 
-# <editor-fold desc="test/train(N-fold) 분할">
-'''
-split_test_train_set(data_path=ROOT_DIR / "attribution" / "data" / "grouped_uid_buckets",
-                     save_path=ROOT_DIR / "attribution" / "data" / "training_set",
-                     fold_size=3, test_size=0.2, random_state=42)
-'''
-# </editor-fold>
-
-# <editor-fold desc="train+val 데이터 생성">
-'''
-train_val_concatenate(data_path=ROOT_DIR/"attribution" / "data",
-                      save_path=ROOT_DIR / "attribution" / "data" / "train")
-'''
-# </editor-fold>
-
-
-
 # <editor-fold desc="float32 --> float16 위해 데이터 매핑 ">
-'''
 map = category_mapping(ROOT_DIR / "attribution" / "data" / "training_set" / "train",
                        ROOT_DIR / "attribution" / "data" / "training_set" / "test",
                        ROOT_DIR / "attribution" / "data" / "training_set",
                        ["campaign","cat1","cat2","cat3","cat4","cat5","cat6","cat7","cat8","cat9"])
-'''
 # </editor-fold>
-
-
 # <editor-fold desc="속도 개선을 위해 파일형식 변경 & 변환 데이터로 저장 ">
 parquet_to_memmap_with_padding(data_path=ROOT_DIR / "attribution" / "data" / "training_set" / "train",
                                mapping_path=ROOT_DIR / "attribution" / "data" / "training_set")
